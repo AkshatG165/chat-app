d42b00fd6f013a4cb3345944b047d74da0c999b4
Gupta <akshat.j.gupta@accenture.com>
2024-03-18 14:41:46 +0530

    feat: Added UI component for making cards

---
+++ components/ui/card.tsx
@@ -0,0 +1,10 @@
+import classes from './card.module.css';
+
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export default function Card({ children, className }: Props) {
+  return <div className={`${classes.card} ${className}`}>{children}</div>;
+}

db45114d8c0765b01c9c7f6a3415c9d98a66cb4a
AkshatG165 <guptaakshat3011@gmail.com>
2024-03-19 18:38:17 +0530

    feat: Added utility functions

---
+++ util/helper.tsx
@@ -0,0 +1,12 @@
+export function formatDate(date: Date) {
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = String(date.getFullYear()).slice(-2);
+  return `${day}/${month}/${year}`;
+}
+
+export function formatTime(date: Date) {
+  return `${date.toLocaleTimeString().slice(0, 4)} ${date
+    .toLocaleTimeString()
+    .slice(-2)}`;
+}

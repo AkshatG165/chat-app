d46fbdcd1c5a00fdedf50d712d69de24d723316a
AkshatG165 <guptaakshat3011@gmail.com>
2024-03-23 03:24:12 +0530

    feat: functions for encrypting & decrypting password

--- util/helper.ts
+++ util/helper.ts
@@ -1,3 +1,5 @@
+import { hash, compare } from 'bcryptjs';
+
 export function formatDate(date: Date) {
   const day = String(date.getDate()).padStart(2, '0');
   const month = String(date.getMonth() + 1).padStart(2, '0');
@@ -8,4 +10,17 @@ export function formatDate(date: Date) {
 export function formatTime(date: Date) {
   const timeArr = date.toLocaleTimeString().split(':');
   return `${timeArr[0]}:${timeArr[1]} ${timeArr[2].split(' ')[1]}`;
+}
+
+export async function hashPassword(password: string) {
+  const hashedPassword = await hash(password, 12);
+  return hashedPassword;
+}
+
+export async function checkPassword(
+  enteredPassword: string,
+  storedPassword: string
+) {
+  const isValid = await compare(enteredPassword, storedPassword);
+  return isValid;
 }

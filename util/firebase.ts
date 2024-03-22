9bddf2fe9d5c14b85c51a56affb4269b4d0bf8be
AkshatG165 <guptaakshat3011@gmail.com>
2024-03-23 02:59:19 +0530

    feat: Connecting to firestore db

---
+++ util/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD95wEk7tUdXNgi4yiU4GVGIaM9o-Cyfmg',
+  authDomain: 'chat-app-e3910.firebaseapp.com',
+  projectId: 'chat-app-e3910',
+  storageBucket: 'chat-app-e3910.appspot.com',
+  messagingSenderId: '676875878460',
+  appId: '1:676875878460:web:cbcc2959e7f97cb7274339',
+  measurementId: 'G-CTS8BRN79J',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);

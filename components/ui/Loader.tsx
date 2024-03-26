ab1b939c21338ac6c73910b8be9aae49d07850c1
AkshatG165 <guptaakshat3011@gmail.com>
2024-03-26 19:17:44 +0530

    feat: Made loader customizable

--- components/ui/Loader.tsx
+++ components/ui/Loader.tsx
@@ -1,11 +1,11 @@
 import classes from './Loader.module.css';
 
-type Props = { color: string; width?: string | number };
+type Props = { color: string; width?: string | number; className?: string };
 
-export default function Loader({ color, width }: Props) {
+export default function Loader({ color, width, className }: Props) {
   return (
     <div
-      className={classes.loader}
+      className={classes.loader + ' ' + className}
       style={{ backgroundColor: color, width: width ? `${width}px` : '25px' }}
     />
   );

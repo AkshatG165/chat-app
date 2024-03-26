0d8ded98b0cf4330aa7e63d9f2e88260d7c43a57
AkshatG165 <guptaakshat3011@gmail.com>
2024-03-26 19:41:30 +0530

    feat: Adding context provider & minor style fixes

--- components/ChatsSection/ChatsSection.tsx
+++ components/ChatsSection/ChatsSection.tsx
@@ -2,20 +2,18 @@ import classes from './ChatsSection.module.css';
 import Card from '../UI/Card';
 import Search from './Search';
 import Chats from './Chats';
-import { FaUserPlus } from 'react-icons/fa';
-import Link from 'next/link';
+import SearchContextProvider from '@/store/contexts/SearchContext';
 
 export default function ChatsSection() {
   return (
-    <Card className={classes.card}>
-      <div className={classes.header}>
-        <p>Messages</p>
-        <Link href="#">
-          <FaUserPlus className={classes.icon} />
-        </Link>
-      </div>
-      <Search />
-      <Chats />
-    </Card>
+    <SearchContextProvider>
+      <Card className={classes.card}>
+        <div className={classes.header}>
+          <p>Messages</p>
+        </div>
+        <Search />
+        <Chats />
+      </Card>
+    </SearchContextProvider>
   );
 }

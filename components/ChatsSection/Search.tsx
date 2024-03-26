78fdc555b7315925776201cbc4018ec810a37a7e
AkshatG165 <guptaakshat3011@gmail.com>
2024-03-26 19:40:36 +0530

    feat: Functionaity to search users

--- components/ChatsSection/Search.tsx
+++ components/ChatsSection/Search.tsx
@@ -1,12 +1,25 @@
+import { useContext } from 'react';
 import classes from './Search.module.css';
 import { GoSearch } from 'react-icons/go';
+import { SearchContext } from '@/store/contexts/SearchContext';
 
 export default function Search() {
+  const searchCtx = useContext(SearchContext);
+
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
+    searchCtx.setSearchTerm(e.currentTarget.value);
+
   return (
     <div className={classes.container}>
       <div className={classes.search}>
         <GoSearch className={classes.icon} />
-        <input id="search" type="text" name="search" placeholder="Search" />
+        <input
+          id="search"
+          type="text"
+          name="search"
+          placeholder="Search for users"
+          onChange={inputHandler}
+        />
       </div>
     </div>
   );

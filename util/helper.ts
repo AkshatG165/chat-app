869df4a903baa614ee1d638d53d3821892298b65
AkshatG165 <guptaakshat3011@gmail.com>
2024-04-06 18:03:24 +0530

    fix: Updated helper function to take timestamp as input

--- util/helper.ts
+++ util/helper.ts
@@ -1,13 +1,15 @@
 import { hash, compare } from 'bcryptjs';
 
-export function formatDate(date: Date) {
+export function formatDate(timestamp: number) {
+  const date = new Date(timestamp);
   const day = String(date.getDate()).padStart(2, '0');
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const year = String(date.getFullYear()).slice(-2);
   return `${day}/${month}/${year}`;
 }
 
-export function formatTime(date: Date) {
+export function formatTime(timestamp: number) {
+  const date = new Date(timestamp);
   const timeArr = date.toLocaleTimeString().split(':');
   return `${timeArr[0]}:${timeArr[1]} ${timeArr[2].split(' ')[1]}`;
 }

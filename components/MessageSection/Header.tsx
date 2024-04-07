9ebd0d3ba49160dec78fb8e9230616d8979bfdbd
AkshatG165 <guptaakshat3011@gmail.com>
2024-04-07 21:53:17 +0530

    feat: Now showing data dynamically based on selected chat

--- components/MessageSection/Header.tsx
+++ components/MessageSection/Header.tsx
@@ -1,31 +1,38 @@
 import classes from './Header.module.css';
-import profilePic from '../../public/pic.jpg';
 import { IoCall } from 'react-icons/io5';
 import { FaVideo } from 'react-icons/fa';
 import { BsThreeDots } from 'react-icons/bs';
-import Image from 'next/image';
 import Link from 'next/link';
-
-const user = {
-  name: 'Akshat Gupta',
-  profilePic,
-  online: true,
-};
+import { useContext } from 'react';
+import { ChatContext } from '@/store/ChatContext';
+import { useSession } from 'next-auth/react';
 
 export default function Header() {
+  const { data: session } = useSession();
+  const chatCtx = useContext(ChatContext);
+
+  const img =
+    session?.user.id === chatCtx.selectedChat?.user1
+      ? chatCtx.selectedChat?.user2Img
+      : chatCtx.selectedChat?.user1Img;
+  const name =
+    session?.user.id === chatCtx.selectedChat?.user1
+      ? chatCtx.selectedChat?.user2Name
+      : chatCtx.selectedChat?.user1Name;
+
   return (
     <div className={classes.header}>
       <div className={classes.left}>
-        <Image
-          src={user.profilePic}
+        <img
+          src={img}
           alt="profile-pic"
           height={50}
           width={50}
           className={classes.profilepic}
         />
         <div className={classes.title}>
-          <p className={classes.name}>{user.name}</p>
-          {user.online && <p className={classes.status}>Online</p>}
+          <p className={classes.name}>{name}</p>
+          {/* {user.online && <p className={classes.status}>Online</p>} */}
         </div>
       </div>
       <div className={classes.actions}>

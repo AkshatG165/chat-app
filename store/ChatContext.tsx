3b90e591adfc679ba0941310149cf2af4345ef35
AkshatG165 <guptaakshat3011@gmail.com>
2024-04-07 21:33:31 +0530

    fix: ChatContext now handels which chat is currently selected

---
+++ store/ChatContext.tsx
@@ -0,0 +1,36 @@
+import React, { createContext, useState } from 'react';
+import { Chat } from '@/model/Chat';
+// import { Message } from '@/model/Message';
+
+// export type State = {
+//   chat: Chat;
+//   message: Message[];
+// };
+
+type Context = {
+  selectedChat: Chat | undefined;
+  setSelectedChat: (chat: Chat) => void;
+};
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const ChatContext = createContext<Context>({
+  selectedChat: undefined,
+  setSelectedChat: (chat: Chat) => {},
+});
+
+export default function ChatContextProvider({ children }: Props) {
+  const [selectedChat, setSelectedChat] = useState<Chat>();
+
+  const handleChatSelect = (chat: Chat) => setSelectedChat(chat);
+
+  return (
+    <ChatContext.Provider
+      value={{ selectedChat, setSelectedChat: handleChatSelect }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+}

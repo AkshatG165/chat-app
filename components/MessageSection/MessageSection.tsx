f753a198d15dde9179e84d307685282b33821fb7
AkshatG165 <guptaakshat3011@gmail.com>
2024-04-08 16:51:42 +0530

    feat: Sending messages to db as well now

--- components/MessageSection/MessageSection.tsx
+++ components/MessageSection/MessageSection.tsx
@@ -7,16 +7,18 @@ import { Message } from '@/model/Message';
 import { ChatContext } from '@/store/ChatContext';
 import Loader from '../UI/Loader';
 
+let isInitial = true;
+
 export default function MessageSection() {
   const [messages, setMessages] = useState<Message[]>();
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(''); //can be '', 'sending', 'getting'
   const [error, setError] = useState<string | null>(null);
-
   const chatCtx = useContext(ChatContext);
 
+  //For getting messages
   useEffect(() => {
     const getMessages = async () => {
-      setLoading(true);
+      setLoading('getting');
       try {
         const res = await fetch(
           `/api/message?chatId=${chatCtx.selectedChat?.id}&count=100`
@@ -28,19 +30,57 @@ export default function MessageSection() {
       } catch (err: any) {
         setError(err);
       }
-      setLoading(false);
+      setLoading('');
     };
 
     getMessages();
   }, [chatCtx.selectedChat]);
 
+  //For sending messages to db
+  useEffect(() => {
+    const sendMessages = async () => {
+      setLoading('sending');
+      const data = {
+        chatId: chatCtx.selectedChat?.id,
+        date: messages![messages!.length - 1].date,
+        from: messages![messages!.length - 1].from,
+        message: messages![messages!.length - 1].message,
+      };
+
+      try {
+        const res = await fetch(`/api/message`, {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        const message = await res.json();
+        if (!res.ok) setError(message);
+      } catch (err: any) {
+        setError(err);
+      }
+      setLoading('');
+    };
+
+    if (!isInitial) {
+      if (
+        messages &&
+        messages.length > 0 &&
+        //because i am setting id as date timestamp of f its not NaN thes its inserted by me & not from db
+        !isNaN(+messages[messages.length - 1].id)
+      )
+        sendMessages();
+    } else isInitial = false;
+  }, [messages]);
+
   return (
     <div className={classes.card}>
       <Header />
-      {loading ? (
+      {loading === 'getting' ? (
         <Loader color="black" width={40} />
       ) : (
-        <Messages messages={messages} />
+        <Messages messages={messages} loading={loading} />
       )}
       <Input setMessages={setMessages} />
     </div>

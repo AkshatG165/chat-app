5051a1bc182e4674a74bf3305e8de105eb3a8016
AkshatG165 <guptaakshat3011@gmail.com>
2024-04-11 15:11:48 +0530

    installed sharp package for image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['firebasestorage.googleapis.com', 'localhost'],
+    domains: [
+      'firebasestorage.googleapis.com',
+      'localhost',
+      'chat-app-sooty-theta.vercel.app',
+    ],
   },
 };
 
